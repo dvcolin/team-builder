fcfe5a22eab012449aba710005201ba410acf712
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-07-24 12:34:44 -0700

    Added and styled form component

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -4,18 +4,80 @@ import './App.css';
 import { theme } from './Theme';
 
 const FormContainer = styled.div`
-    height: 300px;
-    width: 75%;
-    background: green;
+    width: 60%;
+    background: ${theme.colors.lightBlue};
     margin: 0 auto;
     border-radius: 5px;
 `;
 
+const FormContent = styled.form`
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`;
+
+const FormTitle = styled.h2`
+    font-size: 1.8rem;
+    font-family: ${theme.fonts.josefinSans};
+    padding-top: 1.5rem;
+`;
+
+const FormLabel = styled.label`
+    font-size: 1.4rem;
+    text-transform: uppercase;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+    width: 100%;
+    height: 100%;
+    margin-left: 2.5rem;
+    font-family: ${theme.fonts.josefinSans};
+`;
+
+const SubmitButton = styled.button`
+    width: 20%;
+    background: white;
+    border: none;
+    border-radius: 5px;
+    padding: 0.5rem 1.5rem;
+    text-transform: uppercase;
+    margin: 1.5rem auto;
+    font-size: 1.2rem;
+`;
 
 const Form = props => {
+
+    const [user, setUser] = useState({ name: '', email: '', role: '' });
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log('user');
+    }
     return(
         <FormContainer>
+            <FormContent onSubmit={handleSubmit}>
+                <FormTitle>Sign Up</FormTitle>
 
+                <div className='form-field'>
+                    <FormLabel for='name'>Name
+                        <input type ='text'></input>
+                    </FormLabel>
+                </div>
+                <div className='form-field'>
+                    <FormLabel for='email'>Email
+                        <input type ='text'></input>
+                    </FormLabel>
+                </div>
+                <div className='form-field'>
+                    <FormLabel for='role'>Role
+                        <input type ='text'></input>
+                    </FormLabel>
+                </div>
+                <SubmitButton>Submit</SubmitButton>
+            </FormContent>
         </FormContainer>
     );
 }
